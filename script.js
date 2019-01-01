441c4b230cdcd23a24cccc14cd38956981f84fb7
Philip Kiely <kielyphi@grinnell.edu>
2019-01-01 13:11:41 -0600

    title script plus bootstrap lib

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 //Google Docs Init
 //document.getElementById('gDoc').appendChild('p')
 //="<b>1/1/2000 12:00 AM</b>";
+
+function SMSJTitle(){
+    //Uses a typewriter effect to expand the header text
+    var texts = ['Send ', 'My', 'Self ', 'J'];
+    //typing function
+    function type(txt, i=0) {
+        if (i < txt.length) { //with timeout this is a while loop
+            if (i == 0) {
+                document.getElementById("Title"+txt.trim()).innerHTML = "<span id='TitleFirst" + txt.trim() + "'>" + txt.charAt(i) + "</span>";
+                document.getElementById("TitleFirst"+txt.trim()).style.color = "#0362DC"; //iMessage Blue
+            } else {
+                document.getElementById("Title"+txt.trim()).innerHTML += txt.charAt(i);
+            }
+            setTimeout(function(){type(txt, i+1)}, 600);
+        }
+    }
+    //call typing function on each text span
+    function typeCaller(t, i=0){
+        type(t[i]);
+        if (i == 1) {
+            setTimeout(function(){typeCaller(t, i+1)}, 1200);
+        } else if (i < 4) {
+            setTimeout(function(){typeCaller(t, i+1)}, 3000);
+        }
+    }
+    //wait a couple seconds on load.
+    setTimeout(function(){typeCaller(texts);}, 2000);
+}
+
+window.onload = SMSJTitle();
