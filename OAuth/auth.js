e6f82c93fcf21cdaac0f940bafc195cd66b42e13
Alexander Mohn <pogrmman@gmail.com>
2019-01-07 19:41:52 -0600

    OAuth is working to get basic profile info

--- OAuth/auth.js
+++ OAuth/auth.js
@@ -1,41 +1,69 @@
 // Keep everything in a namespace so that there're no conflicts
 var Auth = (function() {
-    var publicFunctions = {};
-    
     var GoogleAuth;
     var isAuthorized = false;
 
     // Load Google API auth2 modules
     function loadClient() {
-        gapi.load("client:auth2", this.initClient);
+				console.log("loadClient()");
+        gapi.load("auth2", initClient);
     }
-        
+
+		// Set GoogleAuth
+		function setAuth() {
+				GoogleAuth = gapi.auth2.getAuthInstance();
+				GoogleAuth.isSignedIn.listen(updateStatus);
+				var status = GoogleAuth.isSignedIn.get();
+				updateStatus(status);
+		}
+		
     // Initialize Google API Client
     function initClient() {
-        gapi.client.init({
-            "clientId": "752165490166-rrh2erpshokg2enq6afu70csmi3h5ikr.apps.googleusercontent.com",
-            "scope": "profile"
-        }).then(function() {
-            this.GoogleAuth = gapi.auth2.getAuthInstance();
-            // Call after_signin after signing in
-            this.GoogleAuth.isSignedIn.listen(updateStatus);
-        });
+        gapi.auth2.init({
+						// This clientId is ONLY to be used in development, not production
+						// To use it, set really-this-is-localhost.com to point to 127.0.0.1 in your hosts file
+						// Run a local webserver (I use the python http.server) and go to really-this-is-localhost.com to test it
+            "clientId": "752165490166-j74ipne57mdk7la4vuv97d2i434ibjar.apps.googleusercontent.com",
+            "scope": "profile",
+						"discoveryDocs": "https://www.googleapis.com/discovery/v1/apis?name=scripts"
+        }).then(setAuth);
     }
 
     // Update Sign In Status
     function updateStatus(signedIn) {
+				var btn = document.getElementById("sign-in");
+				var txt = document.getElementById("text");
+				var info = document.getElementById("info");
         if (signedIn) {
-            this.isAuthorized = true;   
+            isAuthorized = true;
+						btn.onclick = revoke;
+						btn.innerHTML = "Revoke access!";
+						txt.innerHTML = "Authorized!";
+						var user = GoogleAuth.currentUser.get();
+						var profile = user.getBasicProfile();
+						var name = profile.getName();
+						var email = profile.getEmail();
+						info.innerHTML = "Name: " + name + "<br>Email: " + email;
         } else {
-            this.isAuthorized = false;
+						isAuthorized = false;
+						btn.onclick = signIn;
+						btn.innerHTML = "Grant access!";
+						txt.innerHTML = "Unauthorized!";
+						info.innerHTML = " ";
         }
-    }k
+    }
 
-    // Public Handle
-    publicFunctions.signIn = function() {
-        this.loadClient();
-    };
-    
-    return publicFunctions;
+		function revoke() {
+				GoogleAuth.disconnect();
+		}
+		
+		function signIn() {
+				GoogleAuth.signIn();
+		}
+		
+    return {loadClient, signIn};
 })();
-
+Auth.loadClient();
+window.addEventListener('error', function(event) {
+  console.log('err', event);
+});
