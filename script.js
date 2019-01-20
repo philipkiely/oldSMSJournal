7a29f2239de393fb01dbbc74f06b27e0fe6a9d67
Philip Kiely <kielyphi@grinnell.edu>
2019-01-19 18:52:49 -0600

    verify phone number formatting and match

--- script.js
+++ script.js
@@ -39,14 +39,33 @@ function setYear() {
 window.onload = setYear();
 
 function beginSignup() {
-    document.getElementById("signupArea").innerHTML = '<h4 class="card-header">Phone Number</h4><div class="card-body"><p>To get started, please enter and confirm your phone number.</p><form><input class="form-control" type="tel" required id="phoneNumber"><br><input class="form-control" type="tel" required id="phoneNumberConfirm"></form></div><div class="card-footer"><a onclick="getGoogle()" class="btn btn-primary cardButton">Next</a></div>'
+    document.getElementById("signupArea").innerHTML = '<h4 class="card-header">Phone Number</h4><div class="card-body"><p>To get started, please enter and confirm your phone number.</p><div id="noMatch" hidden class="alert alert-danger" role="alert">Error: phone numbers don\'t match.</div><div id="noFormat" hidden class="alert alert-danger" role="alert">Error: phone numbers not recognized.</div><form><input class="form-control" type="tel" required id="phoneNumber"><br><input class="form-control" type="tel" required id="phoneNumberConfirm"></form></div><div class="card-footer"><a onclick="getGoogle()" class="btn btn-primary cardButton">Next</a></div>'
     document.getElementById("phoneNumber").placeholder = "Phone Number";
     document.getElementById("phoneNumberConfirm").placeholder = "Confirm Phone Number";
 }
 
-function getGoogle() {
-    document.getElementById("signupArea").innerHTML = '<h4 class="card-header">Google Account</h4><div class="card-body"><p>What Google Account would you like to keep your journal on?</p><form><input class="form-control" type="text" required id="googleAccount"></form></div><div class="card-footer"><a onclick="getPayment()" class="btn btn-primary cardButton">Next</a></div>'
-    document.getElementById("googleAccount").placeholder = "Google Account";
+function verifyNumber() {
+    phoneNumber = document.getElementById("phoneNumber").value.match(/\d+/g).join(""); //get all numbers regardless of formatting
+    phoneNumberConfirm = document.getElementById("phoneNumberConfirm").value.match(/\d+/g).join("");
+    if ((phoneNumber == phoneNumberConfirm) && (phoneNumber.length == 10)) {
+        return true;
+    }
+    else if (phoneNumber.length == 10) {
+        document.getElementById("noMatch").hidden = false;
+        return false;
+    }
+    else {
+        document.getElementById("noFormat").hidden = false;
+        return false;
+    }
 }
 
-window.onload = alert("SMSJournal is a work in progress. To be notified of its completion, email info@grammiegram.com");
+function getGoogle() {
+    correct = verifyNumber();
+    if (correct) {
+        document.getElementById("signupArea").innerHTML = '<h4 class="card-header">Google Account</h4><div class="card-body"><p>What Google Account would you like to keep your journal on?</p><form><input class="form-control" type="text" required id="googleAccount"></form></div><div class="card-footer"><a onclick="getPayment()" class="btn btn-primary cardButton">Next</a></div>'
+        document.getElementById("googleAccount").placeholder = "Google Account";
+    }
+}
+
+//window.onload = alert("SMSJournal is a work in progress. To be notified of its completion, email info@grammiegram.com");
